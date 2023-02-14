59449303a5a1572a1259905a140704994d07aff6
Nilton Silva <124922390+NiltonSilva10@users.noreply.github.com>
2023-02-14 16:51:25 -0300

    Create script.ino

---
+++ Códigos/14.02/script.ino
@@ -0,0 +1,27 @@
+// https://www.tinkercad.com/things/2gPNFAAz5EI-grand-albar-vihelmo/editel?sharecode=7CT3LQQ9OpbWKJw8ij7f5zwMQZ8z_uHwwdAgsDs2qbY
+
+#include "Adafruit_LEDBackpack.h"
+
+Adafruit_7segment led_display1 = Adafruit_7segment();
+
+int sensorValue = 0;
+
+void setup()
+{
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+  led_display1.begin(112);
+  pinMode(A1, INPUT);
+}
+
+void loop()
+{
+  // read the input on analog pin 0:
+  sensorValue = analogRead(A0);
+  // print out the value you read:
+  //Serial.println(sensorValue);
+  led_display1.println(sensorValue);
+  
+  led_display1.writeDisplay();
+  delay(100); // Wait for 100 millisecond(s)
+}
