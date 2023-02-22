85ae2826b15ebd1bf444955b3541d904ac92b6ae
Thiago Sousa <thiagosousa81@gmail.com>
2023-02-22 14:50:37 -0300

    Update script.ino

--- Códigos/14.02/script.ino
+++ Códigos/14.02/script.ino
@@ -1,11 +1,17 @@
+// Link no Tinkercad
 // https://www.tinkercad.com/things/2gPNFAAz5EI-grand-albar-vihelmo/editel?sharecode=7CT3LQQ9OpbWKJw8ij7f5zwMQZ8z_uHwwdAgsDs2qbY
+
+/* Links importantes
+https://www.todamateria.com.br/seno-cosseno-e-tangente/
+https://brasilescola.uol.com.br/matematica/triangulo-retangulo.htm
+*/
 
 #include "Adafruit_LEDBackpack.h"
 
 Adafruit_7segment led_display1 = Adafruit_7segment();
 
-int sensorValue = 0;
-double distanceM = 5;
+int sensorValue = 0, Pang = 260, angle = 0; //angle é o ângulo máximo do potenciômetro
+double distanceM = 5; // Distância do laser para o receptor estática em metros
 
 void setup()
 {
@@ -22,16 +28,17 @@ void loop()
   // Mostra na tela o valor da leitura:
   //Serial.println(sensorValue);
   
-  if (sensorValue > 170 and sensorValue < 511)
+  angle = (int)(Pang*(float)sensorValue/1023);
+  if (angle <= 90)
   {
-    led_display1.println(sensorValue - 170);
-    led_display1.writeDisplay();
+    led_display1.println(angle);
+  	led_display1.writeDisplay();
   }
   else
   {
     led_display1.println('-0'); // mostra '----' no display
-    led_display1.writeDisplay();
-  }        
+  	led_display1.writeDisplay();
+  }    
   
   delay(100); // Espera 100 milisegundos
 }
