fe3c34ee01d56ee4fcf6fd0ba1488dec7eb89e8e
Nilton Silva <124922390+NiltonSilva10@users.noreply.github.com>
2023-03-14 16:22:49 -0300

    Create script.ino

---
+++ Códigos/14.03/script.ino
@@ -0,0 +1,29 @@
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(A0, INPUT); // Fotorresistor 1
+  pinMode(A1, INPUT); // Fotorresistor 2
+}
+
+void loop()
+{
+  //Leitura do F1
+  Serial.print("Fotorressistor 1 = ");
+  Serial.println(analogRead(A0));
+  
+  // Leitura do F2
+  Serial.print("Fotorressistor 2 = ");
+  Serial.println(analogRead(A1));
+  if ((analogRead(A0)) - (analogRead(A1)) == 0)
+  {
+    Serial.println("Luzes iguais!");
+  }
+  else
+  {
+    Serial.println("Luzes diferentes!");
+  }
+  
+  // O resultado sempre saiu diferente
+  delay(1000);
+}
+
